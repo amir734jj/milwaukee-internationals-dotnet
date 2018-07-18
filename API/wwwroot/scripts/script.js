05467840f76ef337e8cd63fc814e7d4f55ffed67
Seyedamirhossein Hesamian <amirhesamyan@gmail.com>
2018-07-18 00:53:12 -0500

    fixed

--- API/wwwroot/scripts/script.js
+++ API/wwwroot/scripts/script.js
@@ -1,4 +1,14 @@
 angular.module('tourApp', ['ui.toggle'])
     .controller('driverRegistrationCtrl', ['$scope', function() {
         
+    }])
+    .controller('userRegistrationCtrl', ['$scope', function ($scope) {
+        $scope.validateInvitationCode = function ($event) {
+            if ($scope.invitation_code !== $scope.invitation_code_value) {
+                $scope.error = true;
+                $event.preventDefault();
+            } else {
+                $scope.error = false;
+            }
+        }
     }]);
