e04d175671529ea84c2a2cd0cb6905431205268c
Seyedamirhossein Hesamian <amirhesamyan@gmail.com>
2018-07-19 00:33:39 -0500

    Improvement

---
+++ API/Controllers/StudentController.cs
@@ -0,0 +1,46 @@
+﻿using Logic.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace API.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly IStudentLogic _studentLogic;
+
+        /// <summary>
+        /// Constructor dependency injection
+        /// </summary>
+        public StudentController(IStudentLogic studentLogic)
+        {
+            _studentLogic = studentLogic;
+        }
+        
+        /// <summary>
+        /// Returns student view
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("")]
+        [SwaggerOperation("Index")]
+        public IActionResult Index()
+        {
+            return View(_studentLogic.GetAll());
+        }
+        
+        /// <summary>
+        /// Delete a driver
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("Delete/{id}")]
+        [SwaggerOperation("Delete")]
+        public IActionResult Delete(int id)
+        {
+            _studentLogic.Delete(id);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
