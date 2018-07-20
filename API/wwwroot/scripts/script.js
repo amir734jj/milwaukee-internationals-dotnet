b1c0b672db6c0bdcd919fc7075bb50a81cdc2234
Seyedamirhossein Hesamian <amirhesamyan@gmail.com>
2018-07-19 22:37:12 -0500

    WIP

--- API/wwwroot/scripts/script.js
+++ API/wwwroot/scripts/script.js
@@ -12,6 +12,44 @@ angular.module('tourApp', ['ui.toggle', 'ngTagsInput'])
             }
         }
     }])
-    .controller('studentRegistrationCtrl', ['$scope', function () {
-        
+    .controller('studentListCtrl', ['$scope', '$http', function () {
+        $scope.getAllStudentsPDF = function() {
+            $http.get("list/json").then(function(response) {
+                var students = response.data;
+
+                var doc = new jsPDF({
+                    orientation: "l",
+                    lineHeight: 1.5
+                });
+
+                doc.setFont('courier');
+
+                doc.setFontSize(10);
+
+                var subsetAttr = function(attrList, obj) {
+                    return attrList.reduce(function(o, k) {
+                        o[k] = obj[k];
+                        return o;
+                    }, {});
+                };
+
+                var i, j, temparray, chunk = 25;
+                for (i = 0, j = students.length; i < j; i += chunk) {
+                    temparray = students.slice(i, i + chunk);
+
+                    var str = stringTable.create(temparray.map(function(student) {
+                        student.fullname = student.fullname.substring(0, 30);
+                        return subsetAttr(["fullname", "country", "email", "university", "attendance"], student);
+                    }));
+
+                    doc.text(20, 20, str);
+
+                    if (i + chunk < j) {
+                        doc.addPage();
+                    }
+                }
+
+                doc.save("student-list.pdf");
+            });
+        };
     }]);
