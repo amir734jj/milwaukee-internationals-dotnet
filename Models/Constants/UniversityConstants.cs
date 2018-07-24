b719236c6bd5456ed5450c83f25bfe947018b320
Seyedamirhossein Hesamian <amirhesamyan@gmail.com>
2018-07-23 19:57:56 -0500

    Better

---
+++ Models/Constants/UniversityConstants.cs
@@ -0,0 +1,7 @@
+﻿namespace Models.Constants
+{
+    public static class UniversityConstants
+    {
+        public static string[] UniversityNames = {"UWM", "Marquette", "MSOE", "Concordia", "Other"};
+    }
+}
