752eafe6ac59813917975b976ed1659a970b4c9d
Seyedamirhossein Hesamian <amirhesamyan@gmail.com>
2018-08-12 23:56:21 -0500

    Added email utility page

---
+++ API/Controllers/UtilityController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class UtilityController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return
+            View();
+        }
+    }
+}
---
+++ Logic/Extensions/ViewModelExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq.Expressions;
+using InfoViaLinq;
+using Models.Interfaces;
+
+namespace Logic.Extensions
+{
+    public static class ObjectExtension
+    {
+        public static string Name<T>(this Expression<Func<T, object>> obj) where T : IViewModel
+        {
+            var info = InfoViaLinq<T>.New()
+                .PropLambda(obj);
+
+            return info.GetAttribute<DisplayAttribute>().Name ?? info.GetPropertyName();
+        }
+    }
+}
