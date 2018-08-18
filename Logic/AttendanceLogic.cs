1d341b1d2aca9f0f1a0a3fa2b93d481a2ae176f9
Unknown <seyedhesamian@northwesternmutual.com>
2018-08-18 01:14:33 -0500

    Fixed a minor bug with url

--- Logic/AttendanceLogic.cs
+++ Logic/AttendanceLogic.cs
@@ -69,7 +69,7 @@ namespace Logic
         {
             _studentLogic.GetAll().ForEach(x =>
             {
-                var url = $"{ApiConstants.WebSiteApiUrl}/utility/EmailCheckIn/Driver/{x.GetHashCode()}";
+                var url = $"{ApiConstants.WebSiteApiUrl}/utility/EmailCheckIn/Student/{x.GetHashCode()}";
                 
                 _emailServiceApi.SendEmailAsync(x.Email, "Tour Check-In", $@"
                     <h4>Please use this link to check-in</h4>
