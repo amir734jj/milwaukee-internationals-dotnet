94c2e01bfefc0c5b294fabf130dd8590f08aeb87
Unknown <seyedhesamian@northwesternmutual.com>
2018-08-24 00:58:40 -0500

    minor

--- API/Attributes/AuthorizeActionFilter.cs
+++ API/Attributes/AuthorizeActionFilter.cs
@@ -40,7 +40,7 @@ namespace API.Attributes
             }
 
             // Redirect to not-authenticated
-            context.HttpContext.Response.Redirect("Identity/NotAuthenticated");
+            context.HttpContext.Response.Redirect($"{ApiConstants.WebSiteUrl}/Identity/NotAuthenticated");
 
             return Task.CompletedTask;
         }
