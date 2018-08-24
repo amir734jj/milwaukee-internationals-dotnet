42df8b2613478ee62df8e0683699bfd79f77b7f4
Unknown <seyedhesamian@northwesternmutual.com>
2018-08-23 23:26:03 -0500

    fixed the typo

--- Logic/DriverHostMappingLogic.cs
+++ Logic/DriverHostMappingLogic.cs
@@ -124,7 +124,7 @@ namespace Logic
             (await _hostLogic.GetAll()).ForEach(x => _emailServiceApi.SendEmailAsync(new []
             {
                 "amirhesamyan@gmail.com"
-            }, "Tour of Milwaukee - Assigned Students", MessageFunc(x)));
+            }, "Tour of Milwaukee - Assigned Drivers", MessageFunc(x)));
 
             // Return true
             return true;
