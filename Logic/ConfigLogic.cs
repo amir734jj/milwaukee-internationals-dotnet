2a0a693445f3c554aa3f74ee44afe45b7bc8b3df
Amir <seyedhesamian@northwesternmutual.com>
2019-06-10 15:02:10 -0500

    More improvement

--- Logic/ConfigLogic.cs
+++ Logic/ConfigLogic.cs
@@ -9,17 +9,6 @@ namespace Logic
 {
     public class ConfigLogic : IConfigLogic
     {
-        private readonly IStudentLogic _studentLogic;
-        private readonly IDriverLogic _driverLogic;
-        private readonly IHostLogic _hostLogic;
-
-        public ConfigLogic(IStudentLogic studentLogic, IDriverLogic driverLogic, IHostLogic hostLogic)
-        {
-            _studentLogic = studentLogic;
-            _driverLogic = driverLogic;
-            _hostLogic = hostLogic;
-        }
-
         public async Task<YearContextViewModel> ResolveYearContext()
         {
             var years = new List<int> {2018, 2019};
