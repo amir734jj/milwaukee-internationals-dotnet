e5f58d27adaec49e77af931306cbffc196b1576d
Amir <seyedhesamian@northwesternmutual.com>
2019-08-22 08:43:52 -0500

    Even better error message

--- API/Controllers/ErrorController.cs
+++ API/Controllers/ErrorController.cs
@@ -11,7 +11,20 @@ namespace API.Controllers
         [HttpGet]
         public IActionResult Index()
         {
-            return Ok("Something went wrong!");
+            return Ok(@"""
+
+Something went wrong!                          
+
+                  
+ ,adPPYba,   ,adPPYba,  8b,dPPYba,  ,adPPYba,  
+a8""     ""8a a8""     ""8a 88P'    ""8a I8[    """"  
+8b       d8 8b       d8 88       d8  `""Y8ba,   
+""8a,   ,a8"" ""8a,   ,a8"" 88b,   ,a8"" aa    ]8I  
+ `""YbbdP""'   `""YbbdP""'  88`YbbdP""'  `""YbbdP""'  
+                        88                     
+                        88                     
+
+");
         }
     }
 }
