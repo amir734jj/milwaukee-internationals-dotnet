a2da53d141e9f1e8333a28b7d5f85c3cbf260104
Seyedamirhossein Hesamian <seyedhesamian@northwesternmutual.com>
2019-12-30 01:17:20 -0600

    Working profile manager

---
+++ Logic/ProfileLogic.cs
@@ -0,0 +1,40 @@
+using System.Threading.Tasks;
+using Logic.Interfaces;
+using Models.Entities;
+using Models.ViewModels;
+
+namespace Logic
+{
+    public class ProfileLogic : IProfileLogic
+    {
+        private readonly IUserLogic _userLogic;
+
+        public ProfileLogic(IUserLogic userLogic)
+        {
+            _userLogic = userLogic;
+        }
+        
+        public ProfileViewModel ResolveProfile(User user)
+        {
+            return new ProfileViewModel
+            {
+                Id = user.Id,
+                Fullname = user.Fullname,
+                Email = user.Email,
+                PhoneNumber = user.PhoneNumber,
+                Username = user.UserName,
+                Role = user.UserRoleEnum
+            };
+        }
+
+        public async Task UpdateUser(ProfileViewModel profile)
+        {
+            await _userLogic.Update(profile.Id, user =>
+            {
+                user.Fullname = profile.Fullname;
+                user.Email = profile.Email;
+                user.PhoneNumber = profile.PhoneNumber;
+            });
+        }
+    }
+}
