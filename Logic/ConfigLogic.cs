2cd12cb27b90c9161740bb8ec62c7a9f3f9722c3
Seyedamirhossein Hesamian <seyedhesamian@northwesternmutual.com>
2020-01-04 00:03:16 -0600

    Not sure if it helps ...

--- Logic/ConfigLogic.cs
+++ Logic/ConfigLogic.cs
@@ -66,7 +66,7 @@ namespace Logic
             }
             else
             {
-                _logger.LogError("Failed to fetch the config from S3");
+                throw new Exception("Failed to fetch the config from S3");
             }
         }
     }
