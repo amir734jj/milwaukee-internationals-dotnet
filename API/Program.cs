042373b3cdd2556966fd5f50239819041e3f6740
Seyedamirhossein Hesamian <amirhesamyan@gmail.com>
2023-03-05 19:08:35 -0600

    Small modification

--- API/Program.cs
+++ API/Program.cs
@@ -1,7 +1,9 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using System.Net;
 
 namespace API
 {
@@ -20,10 +22,19 @@ namespace API
 
         private static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => webBuilder
-                    .UseKestrel()
-                    .UseIISIntegration()
-                    .UseStartup<Startup>())
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    var portConfig = Environment.GetEnvironmentVariable("PORT");
+                    if (string.IsNullOrEmpty(portConfig) || !int.TryParse(portConfig, out var port))
+                    {
+                        port = 5000;
+                    }
+                    
+                    webBuilder.ConfigureKestrel(serverOptions =>
+                    {
+                        serverOptions.Listen(IPAddress.Any, port);
+                    }).UseStartup<Startup>();
+                })
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .ConfigureLogging(logging => logging.SetMinimumLevel(LogLevel.Error))
                 .ConfigureLogging((hostingContext, logging) =>
