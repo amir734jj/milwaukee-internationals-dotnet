a4e038b0097bcfd0d95af9fac1155216a40182dc
Seyedamirhossein Hesamian <amirhesamyan@gmail.com>
2023-08-28 22:49:14 -0500

    revert back to gmail

--- DAL/ServiceApi/EmailServiceApiApi.cs
+++ DAL/ServiceApi/EmailServiceApiApi.cs
@@ -60,7 +60,7 @@ public class EmailServiceApi : IEmailServiceApi
             
             // construct your email with builder
             var email = new TransactionalEmailBuilder()
-                .WithFrom(new SendContact("yiwagu@hotmail.com"))
+                .WithFrom(new SendContact("tourofmilwaukee@gmail.com"))
                 .WithSubject(emailSubject)
                 .WithHtmlPart(emailHtml)
                 .WithCc(new SendContact(ApiConstants.SiteEmail))
